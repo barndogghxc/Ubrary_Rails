88edc9a9eceb1112bb05fd2ea22160c67ce96fbb
barndogghxc <barndogghxc@yahoo.com>
2018-04-04 22:14:41 -0400

    adding images functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -50,7 +50,7 @@ class BooksController < ApplicationController
 	private
 
 	  def book_params
-	  	params.require(:book).permit(:title, :synopsis, :author, :genre_id)
+	  	params.require(:book).permit(:title, :synopsis, :author, :genre_id, :book_img)
 	  end
 
 	  def find_book
